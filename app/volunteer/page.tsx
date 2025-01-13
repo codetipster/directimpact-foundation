3494054f6a4bb796b2c845f6a54fd340fbc9a753
codetipster <talk2samuelnzekwe@gmail.com>
2025-01-13 21:51:45 +0100

    volunteer error

--- app/volunteer/page.tsx
+++ app/volunteer/page.tsx
@@ -3,11 +3,11 @@ import { VolunteerHero } from '@/components/volunteer/VolunteerHero'
 import { VolunteerMission } from '@/components/volunteer/VolunteerMission'
 
 export const metadata: Metadata = {
-  title: 'Become a Volunteer - HopeHarbor',
+  title: 'Become a Volunteer - DirectImpact Empowerment Foundation',
   description: 'Join our team of volunteers and make a difference in communities worldwide.',
 }
 
-export default function VolunteerPage() {
+export default function Volunteer() {
   return (
     <main>
       <VolunteerHero />
