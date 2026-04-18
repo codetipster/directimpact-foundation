eb74f83a6f1de121268f3eeef7d6a1fb9e6a561d
Ogareuben <ogareuben@directimpactempowerment.org>
2026-04-18 15:49:05 +0100

    fixing metadata 4

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,4 @@
-import { Metadata } from 'next' // 1. Import it
 import HomePageAndAbout from '@/components/HomepageAndAbout'
-
-// 2. ACTUALLY USE IT (This makes the error go away)
-export const metadata: Metadata = {
-  title: 'Direct Impact Empowerment Foundation | Home',
-  description: 'Transforming lives in Nigeria and Switzerland through urgent food aid, medical support, and small business empowerment.',
-  openGraph: {
-    images: ['/dimpact-logo.jpeg'],
-  },
-}
 
 export default function Home() {
   return (
@@ -17,4 +7,3 @@ export default function Home() {
     </main>
   )
 }
-
